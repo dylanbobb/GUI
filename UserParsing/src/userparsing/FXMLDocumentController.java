9a9ce38939f9064d428c9eb28902094835612afb
cstuser <cstuser>
2018-10-15 16:04:13 -0400

    Added Json parcing example with a person

--- UserParsing/src/userparsing/FXMLDocumentController.java
+++ UserParsing/src/userparsing/FXMLDocumentController.java
@@ -5,6 +5,10 @@
  */
 package userparsing;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -21,16 +25,81 @@ import javafx.scene.control.Label;
  * @author cstuser
  */
 public class FXMLDocumentController implements Initializable {
+    String jsonString = readJsonFile("./user.json");
+    JsonElement rootElement = new JsonParser().parse(jsonString);
+    JsonObject rootObject = rootElement.getAsJsonObject();
     
     @FXML
     private Label firstNameLabel;
+    
+    @FXML
+    private Label lastNameLabel;
+    
+    @FXML
+    private Label ageLabel;
+    
+    @FXML
+    private Label addressLabel;
+    
+    @FXML
+    private Label cityLabel;
+    
+    @FXML
+    private Label stateLabel;
+    
+    @FXML
+    private Label postalLabel;
+    
+    @FXML
+    private Label homeLabel;
+    
+    @FXML
+    private Label faxLabel;
+    
+    @FXML
+    private Label genderLabel;
     
     @FXML
     private Button retrieveBtn;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println(readJsonFile("./user.json"));
+        String firstName = rootObject.get("firstName").getAsString();
+        firstNameLabel.setText(firstName);
+        
+        String lastName = rootObject.get("lastName").getAsString();
+        lastNameLabel.setText(lastName);
+        
+        int age = rootObject.get("age").getAsInt();
+        ageLabel.setText(age+"");
+        
+        JsonObject addressObject = rootObject.get("address").getAsJsonObject();
+        
+        String address = addressObject.get("streetAddress").getAsString();
+        addressLabel.setText(address);
+        
+        String city = addressObject.get("city").getAsString();
+        cityLabel.setText(city);
+        
+        String state = addressObject.get("state").getAsString();
+        stateLabel.setText(state);
+        
+        String postal = addressObject.get("postalCode").getAsString();
+        postalLabel.setText(postal);
+        
+        JsonArray phoneArray = rootObject.get("phoneNumber").getAsJsonArray();
+        
+        JsonObject homeObject = phoneArray.get(0).getAsJsonObject();
+        String home = homeObject.get("number").getAsString();
+        homeLabel.setText(home);
+        
+        JsonObject faxObject = phoneArray.get(1).getAsJsonObject();
+        String fax = faxObject.get("number").getAsString();
+        faxLabel.setText(fax);
+        
+        JsonObject genderObject = rootObject.get("gender").getAsJsonObject();
+        String gender = genderObject.get("type").getAsString();
+        genderLabel.setText(gender);
     }
     
     @Override
